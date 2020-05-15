24a5fe5b73043db07da302104a960b8eacc0fdcc
Yaosong Hu <yaosonghu@Yaosongs-MacBook-Pro.local>
2020-05-15 12:06:19 +0800

    Updates

---
+++ precache-manifest.f195540f45184c1dcdb374b41e6c38c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63462ec81942ba889867e148a3a4affc",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "78eda41b06bf7e2ca025",
+    "url": "/Random-Quote-Machine/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "88b6020ac66a70d020a6",
+    "url": "/Random-Quote-Machine/static/js/2.fc9d04aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Machine/static/js/2.fc9d04aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78eda41b06bf7e2ca025",
+    "url": "/Random-Quote-Machine/static/js/main.f4c57b89.chunk.js"
+  },
+  {
+    "revision": "c73f369551bbac5f4a31",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.55c922d4.js"
+  }
+]);
